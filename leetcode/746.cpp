9a92da8be1c967e00d4465add52b8c190c1a5657
Chandan Verma <chandan1912003@gmail.com>
2023-09-23 21:06:32 +0530

    746. Min Cost Climbing Stairs

---
+++ leetcode/746.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minCostClimbingStairs(vector<int>& cost) {
+    int n=cost.size();
+    for(int i=2;i<n;i++)
+    {
+        cost[i]+=min(cost[i-1],cost[i-2]);
+    }
+    return min(cost[n-1],cost[n-2]);
+    
+    }
+
+int main() {
+    vector<int> cost={1,100,1,1,1,100,1,1,100,1};
+    cout<<minCostClimbingStairs(cost);
+    return 0;
+}
