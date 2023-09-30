2f40725d29dac481e690949658437c4e4e5c8f8d
Chandan Verma <chandan1912003@gmail.com>
2023-09-30 19:48:45 +0530

    reverse bit

---
+++ leetcode/190.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t r=0;
+        for(int i=0;i<32;i++){
+            r<<=1;
+            if((n&1)==1){
+                r++;
+            }
+            n>>=1;
+        }
+        return r;
+
+    }
+};
